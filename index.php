c56732006b16c6f252377ba2924f5de1062b44dd
Gaspare <Gas9220@users.noreply.github.com>
2023-04-14 14:21:28 +0200

    Constructor created

--- index.php
+++ index.php
@@ -5,8 +5,16 @@ class Movie {
     private $language;
     private $vote;
     private $minutes;
+    private $over18;
 
-    
+    public function __construct(string $title, string $release_date, string $language, float $vote, float $minutes, bool $over18) {
+        $this->title = $title;
+        $this->release_date = $release_date;
+        $this->$language = $language;
+        $this->vote = $vote;
+        $this->minutes = $minutes;
+        $this->over18 = $over18;
+    }
 }
 
 ?>
