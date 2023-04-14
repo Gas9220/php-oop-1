41a0ea25b8a79d00ce5dc97fe09a33c9715a2960
Gaspare <Gas9220@users.noreply.github.com>
2023-04-14 15:56:40 +0200

    Basic UI setup

--- index.php
+++ index.php
@@ -15,14 +15,24 @@
 
 <body>
     <div id="app">
-        <div></div>
+        <div class="container">
+            <div v-for="movie in movies" class="card-container">
+                <div class="movie-card">
+                    <h3>{{ movie.title }}</h3>
+                    <span>Release date: {{ movie.release_date }}</span>
+                    <span>Language: {{ movie.language }}</span>
+                    <span>Minutes: {{ movie.minutes }}</span>
+                    <span>Vote: {{ movie.vote }}</span>
+                    <span>Over 18: {{ movie.over18 ? 'Yes' : 'No' }}</span>
+                </div>
+            </div>
+        </div>
+
     </div>
 
     <!-- Script -->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.3.1/axios.min.js"
-        integrity="sha512-NbjaUHU8g0+Y8tMcRtIz0irSU3MjLlEdCvp82MqciVF4R2Ru/eaXHDjNSOvS6EfhRYbmQHuznp/ghbUvcC0NVw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.3.1/axios.min.js" integrity="sha512-NbjaUHU8g0+Y8tMcRtIz0irSU3MjLlEdCvp82MqciVF4R2Ru/eaXHDjNSOvS6EfhRYbmQHuznp/ghbUvcC0NVw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="src/js/main.js"></script>
     <!-- /Script -->
 
